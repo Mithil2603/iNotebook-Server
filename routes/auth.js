9bdf2ec75ae4bc340830a919c37e7c1a3d2cf04c
Mithil2603 <mithilsuthar2603@gmail.com>
2025-05-26 19:32:38 +0530

    login route added

--- routes/auth.js
+++ routes/auth.js
@@ -68,12 +68,75 @@ router.post(
           email: user.email,
         },
       });
-      
     } catch (err) {
       console.error(
         `[${new Date().toISOString()}] Error Creating User: `,
         err.message
       );
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Please enter Password").exists(),
+  ],
+  async (req, res) => {
+    try {
+      // if there are errors, return bad requests and errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      // fetching email and password from request body
+      const { email, password } = req.body;
+
+      // check the email exists or not
+      const user = await User.findOne({ email });
+
+      // if email doesn't exists, throw error
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Email or Password is Incorrect!" });
+      }
+
+      // compare password from Database
+      const comparePassword = await bcrypt.compare(password, user.password);
+
+      // if password doesn't match throw same error as email
+      if (!comparePassword) {
+        return res
+          .status(400)
+          .json({ error: "Email or Password is Incorrect!" });
+      }
+
+      // sending jwt token on login success
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const jwtToken = jwt.sign(data, process.env.JWT_SECRET, {
+        expiresIn: "7d", // expires in 7 days
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "Login success!",
+        token: jwtToken,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+      });
+    } catch (err) {
+      console.error(`[${new Date().toISOString()}] Login Error: `, err.message);
       return res.status(500).json({ error: "Internal Server Error" });
     }
   }
